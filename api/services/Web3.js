a37ba75716131e557a60ba0f3a0ea4f463b0b531
时昌雪 <925290164@qq.com>
2018-04-24 09:56:01 +0800

    add line

--- api/services/Web3.js
+++ api/services/Web3.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 // let nodeAddress = "http://101.200.32.35:8545";
-let nodeAddress = "http://localhost:8545";
+// let nodeAddress = "http://localhost:8545";
+let nodeAddress = "http://172.17205.164:8545";//上线后
 // let nodeAddress = "http://39.106.146.18:8545";
 let web3 = new Web3(Web3.providers.givenProvider || new Web3.providers.HttpProvider(nodeAddress));
 module.exports = web3;
